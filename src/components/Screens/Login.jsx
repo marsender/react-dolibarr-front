bc0b7dc6f8f0923bad9e191cb9a6e2d2768cfa82
marsender <didier.corbiere@opale-concept.com>
2024-08-12 20:16:11 +0200

    Cleanup code

--- src/components/Screens/Login.jsx
+++ src/components/Screens/Login.jsx
@@ -30,44 +30,7 @@ const Login = () => {
 				.then(
 					(result) => {
 						if (result.status === 200) {
-							console.log('Login ok with token: %s', result.data.success.token)
-							//this.context.setAuthTokens(result.data.success.token)
-							//api.defaults.headers = {
-							//	DOLAPIKEY: result.data.success.token,
-							//}
-							// api
-							// 	.get('users/info')
-							// 	.then((userInfo) => {
-							// 		const userInfoData = userInfo.data
-							// 		let user = new User(result.data.success.token)
-							// 		user.contact_id = userInfoData.contact_id
-							// 		user.email = userInfoData.email
-							// 		user.entity = userInfoData.entity
-							// 		user.firstname = userInfoData.firstname
-							// 		user.id = userInfoData.id
-							// 		user.lastname = userInfoData.lastname
-							// 		user.login = userInfoData.login
-							// 		user.societe_id = userInfoData.societe_id
-							// 		user.user_mobile = userInfoData.user_mobile
-							// 		user.address = userInfoData.address
-							// 		user.zip = userInfoData.zip
-							// 		user.town = userInfoData.town
-
-							// 		api.get('thirdparties/' + user.entity).then((thridParty) => {
-							// 			const thridPartyData = thridParty.data
-							// 			user.third_party.id = thridPartyData.id
-							// 			user.third_party.name = thridPartyData.name
-							// 			user.third_party.name_alias = thridPartyData.name_alias
-							// 			user.third_party.status = thridPartyData.status
-							// 			user.third_party.code_client = thridPartyData.code_client
-							// 			user.third_party.address = thridPartyData.address
-							// 			user.third_party.zip = thridPartyData.zip
-							// 			user.third_party.town = thridPartyData.town
-							// 			//this.context.setUser(user)
-							// 			//this.props.history.push(this.state.referer)
-							// 		})
-							// 	})
-							// 	.catch((e) => console.log(e))
+							//console.log('Login ok with token: %s', result.data.success.token)
 							dispatch(setLoggedIn(true))
 							dispatch(setUserToken(result.data.success.token))
 						} else {
