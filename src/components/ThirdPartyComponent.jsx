8f851bde0b5520236d0d59c56f1f73b094e1750c
marsender <didier.corbiere@opale-concept.com>
2024-08-15 11:09:51 +0200

    Third parties display REF #12

--- src/components/ThirdPartyComponent.jsx
+++ src/components/ThirdPartyComponent.jsx
@@ -4,8 +4,8 @@ export default function ThirdPartyComponent(item, { detail }) {
 	return (
 		<div className="flex">
 			{/* <div className="w-full flex-none text-sm font-medium text-slate-700 mt-2">{item.code_client}</div> */}
-			<h1 className="flex-auto">{detail ? item.email : <Link to={item.url}>{item.name}</Link>}</h1>
-			<div className="text-sm text-gray-900 dark:text-white">{item.phone}</div>
+			<h1 className="flex-1">{detail ? item.phone : <Link to={item.url}>{item.name}</Link>}</h1>
+			<div className="text-gray-500 dark:text-gray-400">{item.email}</div>
 		</div>
 	)
 }
