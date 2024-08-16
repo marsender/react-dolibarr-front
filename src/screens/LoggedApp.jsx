73d4b9fbf943ff8e6bb1bddd81a450295632cbbc
marsender <didier.corbiere@opale-concept.com>
2024-08-16 19:14:16 +0200

    Add third invoice form REF #18

--- src/screens/LoggedApp.jsx
+++ src/screens/LoggedApp.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import LoggedHome from './LoggedHome'
-import Invoices from './Invoices'
 import Invoice from './Invoice'
+import Invoices from './Invoices'
+import InvoiceForm from './InvoiceForm'
 import Logout from './Logout'
 import Navbar from './Navbar'
 import ThirdParty from './ThirdParty'
+import ThirdPartyForm from './ThirdPartyForm'
 import ThirdParties from './ThirdParties'
 
 const LoggedApp = () => {
@@ -17,8 +19,10 @@ const LoggedApp = () => {
 						<Route path="/" element={<LoggedHome />} />
 						<Route path="/invoices" element={<Invoices />} />
 						<Route path="/invoice/:id" element={<Invoice />} />
+						<Route path="/invoice/add" element={<InvoiceForm />} />
 						<Route path="/thirdparties" element={<ThirdParties />} />
 						<Route path="/thirdparty/:id" element={<ThirdParty />} />
+						<Route path="/thirdparty/add" element={<ThirdPartyForm />} />
 						<Route path="/logout" element={<Logout />} />
 						<Route path="*" element={<Navigate replace to="/" />} />
 					</Routes>
