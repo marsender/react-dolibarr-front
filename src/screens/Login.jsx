182520e2c80e22ac99d91766250f37fc608b97e5
marsender <didier.corbiere@opale-concept.com>
2024-08-16 19:14:38 +0200

    Css style

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -58,7 +58,7 @@ const Login = () => {
 						</div>
 						{error && <div className="error smallMargin-t">{error}</div>}
 						<div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-							<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+							<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{t('label.connection')}</button>
 							<Link to="/" className="ml-4">
 								{t('label.cancel')}
 							</Link>
--- src/screens/UnloggedHome.jsx
+++ src/screens/UnloggedHome.jsx
@@ -11,7 +11,7 @@ const UnloggedHome = () => {
 
 	return (
 		<Link to="/login">
-			<button>{t('label.connection')}</button>
+			<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{t('label.connection')}</button>
 		</Link>
 	)
 }
