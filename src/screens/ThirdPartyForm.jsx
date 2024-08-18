60fd86a40b9035d8ad10a2a70da311ed415ad8ed
marsender <didier.corbiere@opale-concept.com>
2024-08-18 09:59:38 +0200

    Third party form REF #19

--- src/screens/ThirdPartyForm.jsx
+++ src/screens/ThirdPartyForm.jsx
@@ -16,17 +16,13 @@ const ThirdPartyForm = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault()
 		const formData = new FormData(e.target)
-		// console.log(data)
-		// const name = data.get('name')
-		// const email = data.get('email')
-		// const phone = data.get('phone')
 		try {
 			api.createThirdParty(formData).then((data) => {
 				if (typeof data.error !== 'undefined') {
 					setError(t(data.error))
 					return
 				}
-				console.log('Create thirdparty with id: %s', data)
+				//console.log('Create thirdparty with id: %s', data)
 				navigate('/thirdparties')
 			})
 		} catch (err) {
@@ -39,13 +35,15 @@ const ThirdPartyForm = () => {
 		<>
 			<h1 className="flex text-center my-4 text-2xl font-semibold">{t('thirdparty.create')}</h1>
 			<form onSubmit={handleSubmit}>
-				<div className="grid gap-6">
+				<div className="grid gap-6 mb-6">
 					<div>
 						<label htmlFor="name" className="required block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 							{t('thirdparty.name')}
 						</label>
 						<input required type="text" name="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
 					</div>
+				</div>
+				<div className="grid gap-6 mb-6 md:grid-cols-2">
 					<div>
 						<label htmlFor="email" className="required block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 							{t('thirdparty.email')}
@@ -58,12 +56,16 @@ const ThirdPartyForm = () => {
 						</label>
 						<input type="text" name="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
 					</div>
+				</div>
+				<div className="grid gap-6 mb-6">
 					<div>
 						<label htmlFor="address" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 							{t('thirdparty.address')}
 						</label>
 						<input type="text" name="address" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
 					</div>
+				</div>
+				<div className="grid gap-6 mb-6 md:grid-cols-2">
 					<div>
 						<label htmlFor="zip" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 							{t('thirdparty.zip')}
@@ -76,8 +78,10 @@ const ThirdPartyForm = () => {
 						</label>
 						<input type="text" name="town" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
 					</div>
+				</div>
+				<div className="grid gap-6 mb-6">
 					{error && <div className="error smallMargin-t">{error}</div>}
-					<div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+					<div>
 						<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{t('label.add')}</button>
 						<Link to="/thirdparties" className="ml-4">
 							{t('label.cancel')}
