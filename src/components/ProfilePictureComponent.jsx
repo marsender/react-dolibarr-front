2bf9d239b0d2c08851f534376064001e31716693
marsender <didier.corbiere@opale-concept.com>
2024-08-23 17:38:54 +0200

    Add a default profile image REF #28

--- src/components/ProfilePictureComponent.jsx
+++ src/components/ProfilePictureComponent.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
+import profilePicture from '/profile-picture.png'
 
-const ProfilePictureComponent = ({ src, userProfileImage }) => {
-	let imgSrc = null
+const ProfilePictureComponent = ({ userProfileImage }) => {
+	let imgSrc = profilePicture
 	if (typeof userProfileImage !== 'undefined' && userProfileImage) {
 		imgSrc = `data:${userProfileImage.contentType};${userProfileImage.encoding},${userProfileImage.content}`
-	} else if (typeof src !== 'undefined' && src) {
-		imgSrc = src
-	} else {
-		console.log('Missing profile image')
-		return null
 	}
 	return <img className="w-8 h-8 rounded-full" src={imgSrc} />
 }
